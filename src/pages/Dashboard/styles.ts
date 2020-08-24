aa3a646758007232049fca335ba796882c3850c9
Matheus Heck <matheus@conduite.tv>
2020-08-24 15:51:23 +0200

    creates repository page

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-family: Roboto;
@@ -14,7 +18,7 @@ export const Title = styled.h1`
   color: #3a3a3a;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -26,6 +30,15 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+    transition: border-color 2s;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c63030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -44,6 +57,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
@@ -78,7 +97,8 @@ export const Repositories = styled.div`
   }
 
   div {
-    margin-left: 16px;
+    margin: 0 16px;
+    flex: 1;
 
     strong {
       font-size: 20px;
