c1191bf5bcbc8c53be3d530d33f8c395c543fae1
Matheus Heck <matheus@conduite.tv>
2020-10-21 18:58:32 +0200

    .

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import path from 'path';
+const path = require('path');
+const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 17527;
 
 const publicPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(publicPath));
+const port = process.env.PORT || 3000;
 
+app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
+   console.log(`server started on port ${port}`);
 });
