fc2fa8bd64f4c98ec16984643b0cecf0097b40cd
Matheus Heck <matheus@conduite.tv>
2020-10-21 19:24:46 +0200

    .

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState, FormEvent, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-import logo from '../../assets/logo.svg';
+import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories, Error } from './styles';
 
@@ -19,6 +19,7 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
+
   const [repositories, setRepositories] = useState<Repository[]>(() => {
     const storagedRepositories = localStorage.getItem(
       '@GithubExplorer:repositories',
@@ -27,6 +28,7 @@ const Dashboard: React.FC = () => {
     if (storagedRepositories) {
       return JSON.parse(storagedRepositories);
     }
+
     return [];
   });
 
@@ -43,9 +45,10 @@ const Dashboard: React.FC = () => {
     event.preventDefault();
 
     if (!newRepo) {
-      setInputError('Digite o autor/repositório');
+      setInputError('Digite o autor/nome do repositório');
       return;
     }
+
     try {
       const response = await api.get<Repository>(`repos/${newRepo}`);
 
@@ -61,12 +64,13 @@ const Dashboard: React.FC = () => {
 
   return (
     <>
-      <img src={logo} alt="GitHub Explorer" />
+      <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
+
       <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
-          onChange={e => setNewRepo(e.target.value)}
+          onChange={(e) => setNewRepo(e.target.value)}
           placeholder="Digite o nome do repositório"
         />
         <button type="submit">Pesquisar</button>
@@ -75,10 +79,10 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
-        {repositories.map(repository => (
+        {repositories.map((repository) => (
           <Link
             key={repository.full_name}
-            to={`/repository/${repository.full_name}`}
+            to={`/repositories/${repository.full_name}`}
           >
             <img
               src={repository.owner.avatar_url}
